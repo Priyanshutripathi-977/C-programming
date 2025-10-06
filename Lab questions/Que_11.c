bedc7a225848c7f6d90261e14a0cdc758fb1d963
Priyanshu Tripathi <priyanshtripathi977@gmail.com>
2025-10-06 19:01:31 +0530

    que 11 added

--- Lab questions/Que_11.c
+++ Lab questions/Que_11.c
@@ -1,12 +1,49 @@
 #include <stdio.h>
-int main()
-{
-float centigrade,fahrenheit;
 
-printf("Enter the tempreture in centigrade: ");
-scanf("%f",& centigrade);
-fahrenheit=9*(centigrade)/5+32;
-printf("tempreture in fahrenheit is :%f",fahrenheit);
+int main() {
+    float num1, num2, result;
+    char op;
 
-return 0;
+    // Taking input from user
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &op); // Note the space before %c to consume any leftover newline
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    // Perform operation using switch
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else {
+                printf("Error: Division by zero is not allowed!\n");
+                return 0;
+            }
+            printf("Result: %.2f\n", result);
+            break;
+
+        default:
+            printf("Error: Invalid operator!\n");
+    }
+
+    return 0;
 }
